852a38d5fddab9a9495f8c93084bf8b54afd809d
xssdpgy <2234501359@qq.com>
2019-06-02 19:40:16 +0800

    ReadWriteLock 读写锁

---
+++ TestJUC/src/com/zang/juc/TestReadWriteLock.java
@@ -0,0 +1,68 @@
+package com.zang.juc;
+
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/*
+ * 1. ReadWriteLock : 读写锁
+ * 
+ * 写写/读写 需要“互斥”
+ * 读读 不需要互斥
+ * 
+ */
+public class TestReadWriteLock {
+
+	public static void main(String[] args) {
+		ReadWriteLockDemo rw = new ReadWriteLockDemo();
+		//1个线程对数据进行写操作
+		new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				rw.set((int)(Math.random() * 101));
+			}
+		}, "Write:").start();
+		
+		//100个线程对数据进行读操作
+		for (int i = 0; i < 100; i++) {
+			new Thread(new Runnable() {
+				
+				@Override
+				public void run() {
+					rw.get();
+				}
+			}).start();
+		}
+	}
+	
+}
+
+class ReadWriteLockDemo{
+	
+	private int number = 0;
+	//创建ReadWriteLock读写锁对象
+	private ReadWriteLock lock = new ReentrantReadWriteLock();
+	
+	//读
+	public void get(){
+		lock.readLock().lock(); //上锁
+		
+		try{
+			System.out.println(Thread.currentThread().getName() + " : " + number);
+		}finally{
+			lock.readLock().unlock(); //释放锁
+		}
+	}
+	
+	//写
+	public void set(int number){
+		lock.writeLock().lock();
+		
+		try{
+			System.out.println(Thread.currentThread().getName());
+			this.number = number;
+		}finally{
+			lock.writeLock().unlock();
+		}
+	}
+}

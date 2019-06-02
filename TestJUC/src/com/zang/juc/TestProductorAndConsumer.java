1d7960eda71bb7b48e95da55018e88f561a3f1dd
xssdpgy <2234501359@qq.com>
2019-06-02 19:37:58 +0800

    生产者消费者案例-虚假唤醒

---
+++ TestJUC/src/com/zang/juc/TestProductorAndConsumer.java
@@ -0,0 +1,96 @@
+package com.zang.juc;
+//package com.atguigu.juc;
+//
+///*
+// * 生产者和消费者案例
+// */
+//public class TestProductorAndConsumer {
+//
+//	public static void main(String[] args) {
+//		Clerk clerk = new Clerk();
+//		
+//		Productor pro = new Productor(clerk);
+//		Consumer cus = new Consumer(clerk);
+//		
+//		new Thread(pro, "生产者 A").start();
+//		new Thread(cus, "消费者 B").start();
+//		
+//		new Thread(pro, "生产者 C").start();
+//		new Thread(cus, "消费者 D").start();
+//	}
+//	
+//}
+//
+////店员
+//class Clerk{
+//	private int product = 0;
+//	
+//	//进货
+//	public synchronized void get(){//循环次数：0
+//		while(product >= 1){//为了避免虚假唤醒问题，应该总是使用在循环中
+//			System.out.println("产品已满！");
+//			
+//			try {
+//				this.wait();//中断和虚假唤醒都可能发生，所以需要将该方法放在while循环里
+//			} catch (InterruptedException e) {
+//			}
+//			
+//		}
+//		
+//		System.out.println(Thread.currentThread().getName() + " : " + ++product);
+//		//为避免线程不能正常关闭，notifyAll()方法不要放在if...else...中
+//		this.notifyAll();
+//	}
+//	
+//	//卖货
+//	public synchronized void sale(){//product = 0; 循环次数：0
+//		while(product <= 0){//为了避免虚假唤醒问题，应该总是使用在循环中
+//			System.out.println("缺货！");
+//			
+//			try {
+//				this.wait();//中断和虚假唤醒都可能发生，所以需要将该方法放在while循环里
+//			} catch (InterruptedException e) {
+//			}
+//		}
+//		
+//		System.out.println(Thread.currentThread().getName() + " : " + --product);
+//		this.notifyAll();
+//	}
+//}
+//
+////生产者
+//class Productor implements Runnable{
+//	private Clerk clerk;
+//
+//	public Productor(Clerk clerk) {
+//		this.clerk = clerk;
+//	}
+//
+//	@Override
+//	public void run() {
+//		for (int i = 0; i < 20; i++) {
+//			try {
+//				Thread.sleep(200);
+//			} catch (InterruptedException e) {
+//			}
+//			
+//			clerk.get();
+//		}
+//	}
+//}
+//
+////消费者
+//class Consumer implements Runnable{
+//	private Clerk clerk;
+//
+//	public Consumer(Clerk clerk) {
+//		this.clerk = clerk;
+//	}
+//
+//	@Override
+//	public void run() {
+//		for (int i = 0; i < 20; i++) {
+//			clerk.sale();
+//		}
+//	}
+//}
